2a9901a7238ea3a569d4f60bdb65464b3d24cdba
Andres Martinez <Indosmoke310@gmail.com>
2011-09-14 20:26:10 -0700

    added more validation

--- js/main.js
+++ js/main.js
@@ -6,10 +6,24 @@ var forIform = function (data){
 
 $(document).ready(function() { 
 
-       var iform = $('#Form');
-       
+       var iform = $('#Form'),
+           fperrorslink = $('fperrorslink')
+           ;
+            
+            
        iform.validate({
-          invalidHandler: function(form ,validator ){},
+          invalidHandler: function(form ,validator ){
+             fperrorslink.click();
+             var html = '' ;
+             for(var key in validator.submitted ){
+               var label = $('label[for^="'+ key +'"]').not('[generated]');
+               var legend = label.closest('fieldset').find('.ui-controlgroup-label')
+               var fieldName = legend.length ? legend.text() : label.text() ;
+               html += '<li>' + fieldName +'</li>' 
+             };
+             $("#Formpageerrors ul").html(html);
+          },
+          
           submitHandler: function(){
              var data = iform.serializeArray()
           forIform(data);
